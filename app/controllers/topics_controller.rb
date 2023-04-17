d9c30183814b142c0eaf17fcc3303ce14edb5367
sachi <urata986@gmail.com>
2023-04-17 05:57:58 +0000

    edit機能再修正（before_actionを使ってみた）

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   before_action :required_logged_in
+  before_action :set_topic, only: [:edit, :update, :destroy]
   
   def index
     @topics = Topic.all.includes(:favorite_users)
@@ -21,12 +22,9 @@ class TopicsController < ApplicationController
   end
   
   def edit
-    @topic = Topic.find(params[:id])
   end
   
   def update
-     @topic = Topic.find(params[:id])
-     
      if @topic.update(topic_params)
        redirect_to topics_path, success: "投稿を更新しました"
      else
@@ -36,9 +34,7 @@ class TopicsController < ApplicationController
   end
   
   def destroy
-    topic = Topic.find(params[:id])
-    
-    if topic.destroy
+    if @topic.destroy
      redirect_to topics_path
     end
     
@@ -47,5 +43,9 @@ class TopicsController < ApplicationController
   private
   def topic_params
     params.require(:topic).permit(:image, :description)
+  end
+  
+  def set_topic
+    @topic = Topic.find(params[:id])
   end
 end
